415a9808faa6c02adb27f8860a9b3505ac0a6e8d
bung87 <crc32@qq.com>
2020-05-07 12:58:24 +0800

    add import modules

--- src/transpiler.ts
+++ src/transpiler.ts
@@ -3,6 +3,10 @@ import * as indentString from 'indent-string';
 import { fs } from 'memfs';
 import { IWriteStream } from 'memfs/lib/volume';
 import { Program } from 'typescript-estree/dist/estree/spec';
+let modules = new Set<string>();
+function nimModules() {
+  return modules
+}
 
 function tsType2nimType(typeAnnotation: any): string {
   let result: string = ""
@@ -14,6 +18,9 @@ function tsType2nimType(typeAnnotation: any): string {
           result = `"${typeAnnotation.value}"`
           break;
       }
+      break;
+    case parser.AST_NODE_TYPES.MemberExpression:
+      result = `${tsType2nimType(typeAnnotation.object)}.${tsType2nimType(typeAnnotation.property)}`
       break;
     case parser.AST_NODE_TYPES.Identifier:
       if (typeAnnotation.name === "Promise") {
@@ -50,7 +57,66 @@ function tsType2nimType(typeAnnotation: any): string {
         result = `await ${convertCallExpression(typeAnnotation)}`
       }
       break;
+    case parser.AST_NODE_TYPES.ArrowFunctionExpression:
+      const isGenerator = typeAnnotation.generator;
+      const isAsync = typeAnnotation.async;
+      const isExpression = typeAnnotation.expression;
+      const params = typeAnnotation.params;
+      const body = typeAnnotation.body
+      const nimpa = params.map((p: any) => {
+        const name = p.name
+        const typ = p.typeAnnotation ? tsType2nimType(p.typeAnnotation.typeAnnotation) : "auto"
+        return `${name}${typ ? ":" + typ : ""}`
+      })
+      var returnType = "auto"
+      const returnStatement = typeAnnotation.body.body.find((x: any) => x.type === parser.AST_NODE_TYPES.ReturnStatement)
+      if (returnStatement) {
+        if (returnStatement.argument.type === parser.AST_NODE_TYPES.BinaryExpression) {
+          if (-1 !== returnStatement.argument.operator.indexOf("==")) {
+            returnType = "bool"
+          } else if (["+", "-", "*", "/"].includes(returnStatement.argument.operator)) {
+            returnType = "int"
+          }
+        }
+      }
 
+      const pragma = isAsync ? "{.async.}" : ""
+      result += `proc (${nimpa.join(",")}): ${returnType} ${pragma ? pragma + " " : ""}= `
+
+      // @TODO remove top level return variable
+      var current: any
+      while (current = body.body.shift()) {
+        result += tsType2nimType(current)
+      }
+      break;
+    case parser.AST_NODE_TYPES.ReturnStatement:
+      switch (typeAnnotation.argument.type) {
+        case parser.AST_NODE_TYPES.BinaryExpression:
+          var op = ""
+          switch (typeAnnotation.argument.operator) {
+            case "===":
+              op = "=="
+              break;
+            case "!==":
+              op = "!="
+              break;
+            default:
+              op = typeAnnotation.argument.operator
+
+          }
+          result = `${tsType2nimType(typeAnnotation.argument.left)} ${op} ${tsType2nimType(typeAnnotation.argument.right)}`
+          break;
+        case parser.AST_NODE_TYPES.CallExpression:
+          result = `${convertCallExpression(typeAnnotation.argument)}`
+          break;
+        default:
+          console.log("tsType2nimType:ReturnStatement", typeAnnotation)
+          break;
+      }
+      break;
+    default:
+      console.log("tsType2nimType", typeAnnotation)
+      break;
   }
   return result;
 }
@@ -59,8 +125,15 @@ function transCommonMemberExpression(obj: string, mem: string): string {
   var result = ""
   if (mem === "push") {
     result = `${obj}.add`
-  }else if (obj === "fs" && mem === "readFileSync") {
+  } else if (obj === "fs" && mem === "readFileSync") {
     result = `readFile`
+    nimModules().add("os")
+  } else if (mem === "some") {
+    nimModules().add("sequtils")
+    result = `${obj}.any`
+  } else if (mem === "sort") {
+    nimModules().add("algorithm")
+    result = `${obj}.sorted`
   } else {
     result = `${obj}.${mem}`
   }
@@ -69,12 +142,30 @@ function transCommonMemberExpression(obj: string, mem: string): string {
 
 function convertCallExpression(node: any): string {
   let result = ""
-  const theNode = node.expression || node.init || node.argument
+  const theNode = node.expression || node.init || node.argument || node
   switch (theNode.callee.type) {
     case parser.AST_NODE_TYPES.MemberExpression:
       {
-        const obj = theNode.callee.object.name
-        const mem = theNode.callee.property.name
+        let mem: string = ""
+
+        switch (theNode.callee.property.type) {
+          case parser.AST_NODE_TYPES.CallExpression:
+            mem = convertCallExpression(theNode.callee.property)
+            break;
+          default:
+            mem = theNode.callee.property.name
+            break;
+        }
+        var obj: string = ""
+        switch (theNode.callee.object.type) {
+          case parser.AST_NODE_TYPES.CallExpression:
+            obj = convertCallExpression(theNode.callee.object)
+            break;
+          default:
+            obj = theNode.callee.object.name
+            break;
+        }
+
         const args = theNode.arguments.map((x: any) => tsType2nimType(x))
         const func = transCommonMemberExpression(obj, mem);
         result = `${func}(${args.join(",")})`
@@ -103,11 +194,8 @@ function convertVariableDeclarator(node: any): string {
   switch (node.init.type) {
     case parser.AST_NODE_TYPES.CallExpression:
       result = convertCallExpression(node)
-
       break;
     case parser.AST_NODE_TYPES.ArrayExpression:
-      // console.log(node)
-      // const typ = tsType2nimType(node.typeAnnotation)
       const eles = node.init.elements
       result = `@[${eles.map((x: any) => tsType2nimType(x))}]`
       break;
@@ -117,7 +205,7 @@ function convertVariableDeclarator(node: any): string {
 }
 
 function convertVariableDeclaration(node: any): string {
-  const nimKind = node.kind === "const" ? "let" : "var"
+  const nimKind = node.kind === "const" ? "var" : "var"
   const vars = node.declarations.map((x: any) => {
     if (x.id.typeAnnotation) {
       const typ = tsType2nimType(x.id.typeAnnotation.typeAnnotation)
@@ -132,16 +220,14 @@ function convertVariableDeclaration(node: any): string {
 
 class Transpiler {
   constructor(protected ast: Program, protected writer: IWriteStream) {
-
+    modules = new Set()
   }
   writeNode(node: any, indent = 2) {
     switch (node.type) {
       case parser.AST_NODE_TYPES.ExpressionStatement:
         switch (node.expression.type) {
           case parser.AST_NODE_TYPES.CallExpression:
-
             this.writeLine(convertCallExpression(node), indent)
-
             break;
         }
         break;
@@ -158,9 +244,17 @@ class Transpiler {
         })
         break;
       case parser.AST_NODE_TYPES.ReturnStatement:
+        if (node.argument.type === parser.AST_NODE_TYPES.Identifier) {
+          this.writeLine(`return ${node.argument.name}`)
+        } else if (node.argument.type === parser.AST_NODE_TYPES.CallExpression) {
+          this.writeLine(convertCallExpression(node), indent)
+
+        } else {
+          console.log("writeNode:ReturnStatement:", node)
+        }
         break;
       default:
-        console.log(node)
+        console.log("writeNode:", node)
         break;
     }
   }
@@ -191,9 +285,9 @@ class Transpiler {
 
       node.declaration.declarations.map((m: any) => {
         const name = m.id.name;
-        const returnType = tsType2nimType(m.init.returnType.typeAnnotation);
+        const returnType = m.init.returnType ? tsType2nimType(m.init.returnType.typeAnnotation) : "auto";
         const comment = this.getComment(m)?.replace(/^\s*\*+/gm, "").trim() || ""
-        
+
         switch (m.init.type) {
           case parser.AST_NODE_TYPES.ArrowFunctionExpression:
             {
@@ -209,7 +303,7 @@ class Transpiler {
               })
               const pragma = isAsync ? "{.async.}" : ""
               this.writeLine(`proc ${name}*(${nimpa.join(",")}): ${returnType} ${pragma ? pragma + " " : ""}= `, 0)
-              this.writer.write(indentString( "# " + comment.split("\n").join("\n#") + "\n\n", 2))
+              this.writer.write(indentString("# " + comment.split("\n").join("\n#") + "\n\n", 2))
               // @TODO remove top level return variable
               var current: any
               while (current = body.body.shift()) {
@@ -249,6 +343,11 @@ export function transpile(filePath = "/unnamed.nim", code: string, options = { c
     const ast = parser.parse(code, options);
     const transpiler = new Transpiler(ast, writer);
     transpiler.transpile();
+    if (nimModules().size > 0) {
+      const insert = new Buffer("import " + Array.from(nimModules()).join(",") + "\n\n")
+      fs.writeSync(fd, insert, 0, insert.length, 0)
+    }
+
     writer.end()
   })
 
