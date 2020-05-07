579e799fbf4091b561484f028c82d72f045a1849
bung87 <crc32@qq.com>
2020-05-07 16:13:34 +0800

    o

--- __tests__/compiler-spec.ts
+++ __tests__/compiler-spec.ts
@@ -117,4 +117,89 @@ proc filterImagesBySizes*(images:seq[ImageInfo],sizes:seq[int]): auto =
   })
 });
 
+test('Should handle for statement', (done) => {
+  const typedef = `
+/**
+ * Convert a PNG of the byte array to the DIB (Device Independent Bitmap) format.
+ * PNG in color RGBA (and more), the coordinate structure is the Top/Left to Bottom/Right.
+ * DIB in color BGRA, the coordinate structure is the Bottom/Left to Top/Right.
+ * @param src Target image.
+ * @param width The width of the image.
+ * @param height The height of the image.
+ * @param bpp The bit per pixel of the image.
+ * @return Converted image
+ * @see https://en.wikipedia.org/wiki/BMP_file_format
+ */
+const convertPNGtoDIB = (
+  src: Buffer,
+  width: number,
+  height: number,
+  bpp: number
+) => {
+  const cols = width * bpp
+  const rows = height * cols
+  const rowEnd = rows - cols
+  const dest = Buffer.alloc(src.length)
+
+  for (let row = 0; row < rows; row += cols) {
+    for (let col = 0; col < cols; col += bpp) {
+      // RGBA: Top/Left -> Bottom/Right
+      let pos = row + col
+      const r = src.readUInt8(pos)
+      const g = src.readUInt8(pos + 1)
+      const b = src.readUInt8(pos + 2)
+      const a = src.readUInt8(pos + 3)
+
+      // BGRA: Right/Left -> Top/Right
+      pos = rowEnd - row + col
+      dest.writeUInt8(b, pos)
+      dest.writeUInt8(g, pos + 1)
+      dest.writeUInt8(r, pos + 2)
+      dest.writeUInt8(a, pos + 3)
+    }
+  }
+
+  return dest
+}
+`
+  const expected = `proc convertPNGtoDIB(src:Buffer,width:int,height:int,bpp:int): auto = 
+  ## Convert a PNG of the byte array to the DIB (Device Independent Bitmap) format.
+  ## PNG in color RGBA (and more), the coordinate structure is the Top/Left to Bottom/Right.
+  ## DIB in color BGRA, the coordinate structure is the Bottom/Left to Top/Right.
+  ## @param src Target image.
+  ## @param width The width of the image.
+  ## @param height The height of the image.
+  ## @param bpp The bit per pixel of the image.
+  ## @return Converted image
+  ## @see https://en.wikipedia.org/wiki/BMP_file_format
+
+  var cols = width * bpp
+  var rows = height * cols
+  var rowEnd = rows - cols
+  var dest = Buffer.alloc(src.length)
+  var row = 0
+  while row < rows:
+    var col = 0
+    while col < cols:
+      var pos = row + col
+      var r = src.readUInt8(pos)
+      var g = src.readUInt8(pos + 1)
+      var b = src.readUInt8(pos + 2)
+      var a = src.readUInt8(pos + 3)
+      dest.writeUInt8(b,pos)
+      dest.writeUInt8(g,pos + 1)
+      dest.writeUInt8(r,pos + 2)
+      dest.writeUInt8(a,pos + 3)
+  return dest
+`
+  const result = transpile(undefined, typedef)
+
+  result.on("close", () => {
+    expect(fs.readFileSync(result.path).toString()).toBe(expected);
+    done()
+
+  })
+});
+
+
 
