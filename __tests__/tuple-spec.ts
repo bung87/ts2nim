445094a4cee5210ef0fe9ada0079262eca03fc61
bung87 <crc32@qq.com>
2020-05-12 15:54:52 +0800

    add tuple

---
+++ __tests__/tuple-spec.ts
@@ -0,0 +1,21 @@
+import { transpile } from '../src/transpiler';
+import { fs } from 'memfs';
+test('Should handle tuple', done => {
+const typedef = `
+// Declare a tuple type
+let x: [string, number];
+// Initialize it
+x = ['hello', 10]; // OK
+`;
+  const expected = `## Declare a tuple type
+var x:(string,int)
+## Initialize it
+x = @["hello",10]
+`;
+  const result = transpile(undefined, typedef);
+
+  result.on('close', () => {
+    expect(fs.readFileSync(result.path).toString()).toBe(expected);
+    done();
+  });
+});
