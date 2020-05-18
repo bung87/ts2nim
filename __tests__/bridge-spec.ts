ba74552c06b916a091e261285acbd1911bdd4b7a
bung87 <crc32@qq.com>
2020-05-18 16:11:26 +0800

    init bridge

---
+++ __tests__/bridge-spec.ts
@@ -0,0 +1,26 @@
+import { transpile } from '../src/transpiler';
+import { fs } from 'memfs';
+test('Should handle .d.ts', done => {
+  const typedef = `
+    interface Console {
+         Console: NodeJS.ConsoleConstructor;
+       
+        assert(value: any, message?: string, ...optionalParams: any[]): void;
+    }
+    declare var console: Console;
+    `;
+  const expected = `type Console* = ref object of RootObj
+  ## Console*:NodeJS.ConsoleConstructor
+
+
+proc assert*(self:Console) {.importcpp,varargs.} 
+
+var console* {.importc, nodecl.}:Console
+`;
+  const { writer } = transpile('global.d.ts', typedef);
+
+  writer.on('close', () => {
+    expect(fs.readFileSync(writer.path).toString()).toBe(expected);
+    done();
+  });
+});
