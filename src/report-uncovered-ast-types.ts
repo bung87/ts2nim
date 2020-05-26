6ff12ccd008a4ecab049e5d63f5a8f4550b96922
bung87 <crc32@qq.com>
2020-05-26 08:36:51 +0800

    update dep, catch error

--- src/report-uncovered-ast-types.ts
+++ src/report-uncovered-ast-types.ts
@@ -1,12 +1,11 @@
-
 import * as realfs from 'fs';
 import * as path from 'path';
 import * as parser from '@typescript-eslint/typescript-estree';
-import * as process from 'process'
-const dataPath = path.join(__dirname,'transpiler.ts')
-const source = realfs.readFileSync(dataPath).toString()
-for(const typ in parser.AST_NODE_TYPES){
-    if(!source.includes(typ)){
-        process.stdout.write(`\x1b[32m${typ}\n\x1b[0m`)
-    }
+import * as process from 'process';
+const dataPath = path.join(__dirname, 'transpiler.ts');
+const source = realfs.readFileSync(dataPath).toString();
+for (const typ in parser.AST_NODE_TYPES) {
+  if (!source.includes(typ)) {
+    process.stdout.write(`\x1b[32m${typ}\n\x1b[0m`);
+  }
 }
