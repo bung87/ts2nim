9dfa05d7c672a61f3dd09fbfa852b16e38c9b358
bung87 <crc32@qq.com>
2020-05-28 16:19:31 +0800

    enum with comment,fix isPub when class parameter

--- __tests__/enum-spec.ts
+++ __tests__/enum-spec.ts
@@ -7,13 +7,56 @@ enum Color {Red, Green, Blue}
 enum Color {Red = 1, Green = 2, Blue = 4}
 `;
   const expected = `type Color = enum
-  Red = 1, Green, Blue
+  Red = 1
+  Green
+  Blue
+
 
 type Color = enum
-  Red, Green, Blue
+  Red
+  Green
+  Blue
+
 
 type Color = enum
-  Red = 1, Green = 2, Blue = 4
+  Red = 1
+  Green = 2
+  Blue = 4
+
+
+`;
+  const { writer } = transpile(undefined, typedef);
+
+  writer.on('close', () => {
+    expect(fs.readFileSync(writer.path).toString()).toBe(expected);
+    done();
+  });
+});
+
+test('Should handle enum with comment', done => {
+  const typedef = `
+  const enum CharCode {
+    Null = 0,
+    /**
+     * The \`\\t\` character.
+     */
+    Tab = 9,
+    /**
+     * The \`\\n\` character.
+     */
+    LineFeed = 10,
+    /**
+     * The \`\\r\` character.
+     */
+      CarriageReturn = 13
+  }
+  `;
+  const expected = `type CharCode = enum
+  Null = 0
+  Tab = 9   ## The \`\\t\` character.
+  LineFeed = 10   ## The \`\\n\` character.
+  CarriageReturn = 13   ## The \`\\r\` character.
+
 
 `;
   const { writer } = transpile(undefined, typedef);
